650e7665170cc5d14dac3f78dcd7bd4fb0cc9045
CenTdemeern1 <timo.herngreen@gmail.com>
2022-07-16 18:29:08 +0200

    I keep forgetting to commit stuff

--- Game.gd
+++ Game.gd
@@ -4,6 +4,7 @@ enum State {
 	AIMING
 	ROLLING
 	RANDOMNESS
+	PLACE_BALL
 }
 
 enum Players {
@@ -33,13 +34,27 @@ func advance_turn():
 		whose_turn = Players.ONE
 	else:
 		assert(false) # What
+	reset_turn_state()
+
+func reset_turn_state():
 	game_state = State.AIMING
+	$PoolCue/AnimationPlayer.play("Appear")
+	$ViewportView/Viewport/ThreeD/WhiteBall.move_cue = true
+	if player_two_is_bot and whose_turn == Players.TWO:
+		$ViewportView/Viewport/ThreeD/WhiteBall.rotate_cue = false
+	else:
+		$ViewportView/Viewport/ThreeD/WhiteBall.rotate_cue = true
 
 func _process(_delta):
 	if game_state == State.AIMING:
-		if Input.is_action_just_pressed("Click"):
-			$ViewportView/Viewport/ThreeD/WhiteBall.shoot_towards_mouse()
-			game_state = State.ROLLING
+		if player_two_is_bot and whose_turn == Players.TWO:
+			$ViewportView/Viewport/ThreeD/WhiteBall.move_cue = false
+		else:
+			if Input.is_action_just_pressed("Click"):
+				$ViewportView/Viewport/ThreeD/WhiteBall.shoot_towards_mouse()
+				game_state = State.ROLLING
+				$ViewportView/Viewport/ThreeD/WhiteBall.move_cue = false
+				$PoolCue/AnimationPlayer.play("Hit")
 	if game_state == State.ROLLING:
 		var still = true
 		for ball in get_tree().get_nodes_in_group("Ball"):
@@ -48,4 +63,23 @@ func _process(_delta):
 		if still:
 			for ball in get_tree().get_nodes_in_group("Ball"):
 				ball.linear_velocity=Vector3.ZERO
-			advance_turn()
+				game_state = State.RANDOMNESS
+	if game_state == State.RANDOMNESS:
+		var opts : Array = [
+			$ViewportView/Viewport/ThreeD/WhiteBall.transform.basis.x.y,
+			$ViewportView/Viewport/ThreeD/WhiteBall.transform.basis.y.y,
+			$ViewportView/Viewport/ThreeD/WhiteBall.transform.basis.z.y,
+			-$ViewportView/Viewport/ThreeD/WhiteBall.transform.basis.x.y,
+			-$ViewportView/Viewport/ThreeD/WhiteBall.transform.basis.y.y,
+			-$ViewportView/Viewport/ThreeD/WhiteBall.transform.basis.z.y
+		]
+		var table = {
+			$ViewportView/Viewport/ThreeD/WhiteBall.transform.basis.x.y:  4,
+			$ViewportView/Viewport/ThreeD/WhiteBall.transform.basis.y.y:  5,
+			$ViewportView/Viewport/ThreeD/WhiteBall.transform.basis.z.y:  1,
+			-$ViewportView/Viewport/ThreeD/WhiteBall.transform.basis.x.y: 3,
+			-$ViewportView/Viewport/ThreeD/WhiteBall.transform.basis.y.y: 2,
+			-$ViewportView/Viewport/ThreeD/WhiteBall.transform.basis.z.y: 6
+		}
+		print(table[opts.max()])
+		advance_turn()
--- WhiteBall.gd
+++ WhiteBall.gd
@@ -1,9 +1,22 @@
 extends RigidBody
 
+var move_cue = true
+var rotate_cue = true
 var speed_multiplier = 4
+var speed_cap = 8
+
+func _process(_delta):
+	if !move_cue:
+		return
+	var pos = get_viewport().get_camera().unproject_position(translation)
+	pos.y = 1069 - pos.y
+	$"../../../../PoolCue".position = pos
+	$"../../../../PoolCue".look_at($"../../../..".get_global_mouse_position())
 
 func shoot_towards_mouse():
-	var projected_mouse = get_viewport().get_camera().project_position(get_viewport().get_mouse_position(), 0)
+	var mp = $"../../../..".get_local_mouse_position()
+	mp.y = 1069 - mp.y
+	var projected_mouse = get_viewport().get_camera().project_position(mp, 0)
 	# Since we are projecting with zero Z-depth we should make sure we don't shoot the ball upwards by not affecting the Y axis
-	linear_velocity.x = (projected_mouse.x-translation.x) * speed_multiplier
-	linear_velocity.z = (projected_mouse.z-translation.z) * speed_multiplier
+	linear_velocity.x = min(speed_cap, (projected_mouse.x-translation.x) * speed_multiplier)
+	linear_velocity.z = min(speed_cap, (projected_mouse.z-translation.z) * speed_multiplier)
