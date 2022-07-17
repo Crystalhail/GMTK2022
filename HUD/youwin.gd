cac9831ba2d38b2a90bea8fcdd9d7dedbc7a6740
Silver_Volt4 <gdpancake5@gmail.com>
2022-07-17 14:21:41 +0200

    you win / lose

---
+++ HUD/youwin.gd
@@ -0,0 +1,22 @@
+extends ColorRect
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$AnimationPlayer.play("appear")
+	yield(get_tree().create_timer(1),"timeout")
+	$exit.show()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_exit_pressed():
+	get_tree().change_scene("res://HUD/Mainmenu.tscn")
